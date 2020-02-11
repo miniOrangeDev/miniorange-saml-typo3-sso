b2344b2c451cbc6aaf7fa62d03c5f1ccc53d3cfd
Sunny Katiyar <sunny@xecurify.com>
2020-02-11 15:20:52 +0530

    some code cleanup

--- sso/classes/CustomerSaml.php
+++ sso/classes/CustomerSaml.php
@@ -45,12 +45,10 @@ class CustomerSaml{
             curl_setopt ( $ch, CURLOPT_POST, true );
             curl_setopt ( $ch, CURLOPT_POSTFIELDS, $field_string );
             $content = curl_exec ( $ch );
-
+            error_log("create_customer response : ".$content);
             if (curl_errno ( $ch )) {
-
                 // if($this->is_connection_issue(curl_errno($ch)))
                 //     wp_die("There was an issue connection to Internet. Check if your firewall is allowing outbound connection to port 443.<br><br>In case you are using proxy, go to proxy tab in plugin and configure proxy settings.");
-
                 echo 'Request Error:' . curl_error ( $ch );
                 exit ();
             }
@@ -63,21 +61,21 @@ class CustomerSaml{
         {
             $this->objectManager = GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
 
-					error_log(" TYPO3 SUPPORT QUERY : ");
+            error_log(" TYPO3 SUPPORT QUERY : ");
 
-				  	sendMail:
-                         $url = 'https://login.xecurify.com/moas/api/notify/send';
-					  $ch = curl_init($url);
+            sendMail:
+                 $url = 'https://login.xecurify.com/moas/api/notify/send';
+              $ch = curl_init($url);
 
-				  	$subject = "TYPO3 miniOrange SAML SP Plugin Support Query";
+            $subject = "TYPO3 miniOrange SAML SP Plugin Support Query";
 
-				  	$customerKey = Utilities::fetch_cust(Constants::CUSTOMER_KEY);
-				  	$apiKey      = Utilities::fetch_cust(Constants::CUSTOMER_API_KEY);;
+            $customerKey = Utilities::fetch_cust(Constants::CUSTOMER_KEY);
+            $apiKey      = Utilities::fetch_cust(Constants::CUSTOMER_API_KEY);;
 
-				  	if($customerKey==""){
-							$customerKey = "16555";
-							$apiKey = "fFd2XcvTGDemZvbw1bcUesNJWEqKbbUq";
-						}
+            if($customerKey==""){
+                    $customerKey = "16555";
+                    $apiKey = "fFd2XcvTGDemZvbw1bcUesNJWEqKbbUq";
+            }
 
             $currentTimeInMillis = round(microtime(true) * 1000);
             $stringToHash = $customerKey . number_format($currentTimeInMillis, 0, '', '') . $apiKey;
@@ -105,34 +103,33 @@ class CustomerSaml{
                 ),
             );
 
-						error_log(" After Settings Parameters : TYPO3 support content : ".print_r($content,true));
+			error_log("TYPO3 support content : ".print_r($content,true));
 
-					curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json", $customerKeyHeader,
-							$timestampHeader, $authorizationHeader));
-
-					$field_string = json_encode($fields);
-					curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-					curl_setopt($ch, CURLOPT_ENCODING, "");
-          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-          curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-          curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-					curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);# required for https urls
-          curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
-          curl_setopt($ch, CURLOPT_POST, true);
-          curl_setopt($ch, CURLOPT_POSTFIELDS, $field_string);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json", $customerKeyHeader,
+                    $timestampHeader, $authorizationHeader));
+            $field_string = json_encode($fields);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($ch, CURLOPT_ENCODING, "");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		  			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);# required for https urls
+            curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $field_string);
 
           $content = curl_exec($ch);
+          error_log("submit_contact rsponse : ".$content);
+          if (curl_errno($ch)) {
+                $message = GeneralUtility::makeInstance(FlashMessage::class,'CURL ERROR','Error',FlashMessage::ERROR,true);
+                $out = GeneralUtility::makeInstance(ListRenderer ::class)->render([$message]);
+                echo $out;
+                return;
+           }
 
-					  if (curl_errno($ch)) {
-							$message = GeneralUtility::makeInstance(FlashMessage::class,'CURL ERROR','Error',FlashMessage::ERROR,true);
-							$out = GeneralUtility::makeInstance(ListRenderer ::class)->render([$message]);
-							echo $out;
-							return;
-            }
+          curl_close($ch);
+          return $content;
 
-            curl_close($ch);
-
-          return $content ;
         }
 
         function check_customer($email,$password) {
@@ -162,6 +159,7 @@ class CustomerSaml{
             curl_setopt ( $ch, CURLOPT_POSTFIELDS, $field_string );
 
             $content = curl_exec ( $ch );
+            error_log("check_customer response : ".$content);
             if (curl_errno ( $ch )) {
                 echo 'Error in sending curl Request';
                 exit ();
@@ -179,6 +177,7 @@ class CustomerSaml{
                 'email' => $email,
                 'password' => $password
             );
+
             $field_string = json_encode ( $fields );
 
             curl_setopt ( $ch, CURLOPT_FOLLOWLOCATION, true );
@@ -198,6 +197,8 @@ class CustomerSaml{
 
             $content = curl_exec ( $ch );
 
+            error_log("get_customer_key response : ".$content);
+
             if (curl_errno ( $ch )) {
                 echo 'Error in sending curl Request';
                 exit ();
@@ -207,116 +208,116 @@ class CustomerSaml{
             return $content;
         }
 
-        function mo_saml_vl($customerKey,$apiKey,$code,$active) {
-            $url = "";
-            if($active)
-                $url = 'https://login.xecurify.com/moas/api/backupcode/check';
-            else
-                $url = 'https://login.xecurify.com/moas/api/backupcode/verify';
-            
-            $ch = curl_init ( $url );
-            
-            /* The customer Key provided to you */
-//            $customerKey = DB::get_option ( 'mo_saml_admin_customer_key' );
-////
-////            /* The customer API Key provided to you */
-////            $apiKey = DB::get_option ( 'mo_saml_admin_api_key' );
-            
-            /* Current time in milliseconds since midnight, January 1, 1970 UTC. */
-            $currentTimeInMillis = round ( microtime ( true ) * 1000 );
-            
-            /* Creating the Hash using SHA-512 algorithm */
-            $stringToHash = $customerKey . number_format ( $currentTimeInMillis, 0, '', '' ) . $apiKey;
-            $hashValue = hash ( "sha512", $stringToHash );
-            
-            $customerKeyHeader = "Customer-Key: " . $customerKey;
-            $timestampHeader = "Timestamp: " . number_format ( $currentTimeInMillis, 0, '', '' );
-            $authorizationHeader = "Authorization: " . $hashValue;
-            
-            $fields = '';
-            
-            // *check for otp over sms/email
-            
-            $fields = array (
-                    'code' => $code ,
-                    'customerKey' => $customerKey,
-                    'additionalFields' => array(
-                        'field1' => $this->saml_get_current_domain()
-                    )
-                    
-            );
+//        function mo_saml_vl($customerKey,$apiKey,$code,$active) {
+//            $url = "";
+//            if($active)
+//                $url = 'https://login.xecurify.com/moas/api/backupcode/check';
+//            else
+//                $url = 'https://login.xecurify.com/moas/api/backupcode/verify';
+//
+//            $ch = curl_init ( $url );
+//
+//            /* The customer Key provided to you */
+////            $customerKey = DB::get_option ( 'mo_saml_admin_customer_key' );
+//////
+//////            /* The customer API Key provided to you */
+//////            $apiKey = DB::get_option ( 'mo_saml_admin_api_key' );
+//
+//            /* Current time in milliseconds since midnight, January 1, 1970 UTC. */
+//            $currentTimeInMillis = round ( microtime ( true ) * 1000 );
+//
+//            /* Creating the Hash using SHA-512 algorithm */
+//            $stringToHash = $customerKey . number_format ( $currentTimeInMillis, 0, '', '' ) . $apiKey;
+//            $hashValue = hash ( "sha512", $stringToHash );
+//
+//            $customerKeyHeader = "Customer-Key: " . $customerKey;
+//            $timestampHeader = "Timestamp: " . number_format ( $currentTimeInMillis, 0, '', '' );
+//            $authorizationHeader = "Authorization: " . $hashValue;
+//
+//            $fields = '';
+//
+//            // *check for otp over sms/email
+//
+//            $fields = array (
+//                    'code' => $code ,
+//                    'customerKey' => $customerKey,
+//                    'additionalFields' => array(
+//                        'field1' => $this->saml_get_current_domain()
+//                    )
+//
+//            );
+//
+//
+//            $field_string = json_encode ( $fields );
+//
+//            curl_setopt ( $ch, CURLOPT_FOLLOWLOCATION, true );
+//            curl_setopt ( $ch, CURLOPT_ENCODING, "" );
+//            curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
+//            curl_setopt ( $ch, CURLOPT_AUTOREFERER, true );
+//            curl_setopt ( $ch, CURLOPT_SSL_VERIFYPEER, false ); // required for https urls
+//            curl_setopt ( $ch, CURLOPT_SSL_VERIFYHOST, false );
+//            curl_setopt ( $ch, CURLOPT_MAXREDIRS, 10 );
+//            curl_setopt ( $ch, CURLOPT_HTTPHEADER, array (
+//                    "Content-Type: application/json",
+//                    $customerKeyHeader,
+//                    $timestampHeader,
+//                    $authorizationHeader
+//            ) );
+//            curl_setopt ( $ch, CURLOPT_POST, true );
+//            curl_setopt ( $ch, CURLOPT_POSTFIELDS, $field_string );
+//            curl_setopt ( $ch, CURLOPT_CONNECTTIMEOUT, 5 );
+//            curl_setopt ( $ch, CURLOPT_TIMEOUT, 20 );
+//
+//            $content = curl_exec ( $ch );
+//
+//            if (curl_errno ( $ch )) {
+//                echo 'Error in sending curl Request';
+//                exit ();
+//            }
+//
+//            curl_close ( $ch );
+//            return $content;
+//        }
 
-            
-            $field_string = json_encode ( $fields );
-            
-            curl_setopt ( $ch, CURLOPT_FOLLOWLOCATION, true );
-            curl_setopt ( $ch, CURLOPT_ENCODING, "" );
-            curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
-            curl_setopt ( $ch, CURLOPT_AUTOREFERER, true );
-            curl_setopt ( $ch, CURLOPT_SSL_VERIFYPEER, false ); // required for https urls
-            curl_setopt ( $ch, CURLOPT_SSL_VERIFYHOST, false );
-            curl_setopt ( $ch, CURLOPT_MAXREDIRS, 10 );
-            curl_setopt ( $ch, CURLOPT_HTTPHEADER, array (
-                    "Content-Type: application/json",
-                    $customerKeyHeader,
-                    $timestampHeader,
-                    $authorizationHeader 
-            ) );
-            curl_setopt ( $ch, CURLOPT_POST, true );
-            curl_setopt ( $ch, CURLOPT_POSTFIELDS, $field_string );
-            curl_setopt ( $ch, CURLOPT_CONNECTTIMEOUT, 5 );
-            curl_setopt ( $ch, CURLOPT_TIMEOUT, 20 );
-            
-            $content = curl_exec ( $ch );
-    
-            if (curl_errno ( $ch )) {
-                echo 'Error in sending curl Request';
-                exit ();
-            }
-            
-            curl_close ( $ch );
-            return $content;
-        }
-
-        function check_customer_ln($customerKey,$apiKey){
-		
-            $url = 'https://login.xecurify.com/moas/rest/customer/license';
-            $ch = curl_init($url);
-            //$customerKey = DB::get_option ( 'mo_saml_admin_customer_key' );
-            
-            //$apiKey = DB::get_option ( 'mo_saml_admin_api_key' );
-            $currentTimeInMillis = round(microtime(true) * 1000);
-            $stringToHash = $customerKey . number_format($currentTimeInMillis, 0, '', '') . $apiKey;
-            $hashValue = hash("sha512", $stringToHash);
-            $customerKeyHeader = "Customer-Key: " . $customerKey;
-            $timestampHeader = "Timestamp: " . $currentTimeInMillis;
-            $authorizationHeader = "Authorization: " . $hashValue;
-            $fields = '';
-            $fields = array(
-                'customerId' => $customerKey,
-                'applicationName' => 'php_saml_connector_premium_plan'
-            );
-            $field_string = json_encode($fields);
-            curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
-            curl_setopt( $ch, CURLOPT_ENCODING, "" );
-            curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-            curl_setopt( $ch, CURLOPT_AUTOREFERER, true );
-            curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );  
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);  # required for https urls
-            curl_setopt( $ch, CURLOPT_MAXREDIRS, 10 );
-            curl_setopt( $ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json", $customerKeyHeader, $timestampHeader, $authorizationHeader));
-            curl_setopt( $ch, CURLOPT_POST, true);
-            curl_setopt( $ch, CURLOPT_POSTFIELDS, $field_string);
-            curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, 5);
-            curl_setopt( $ch, CURLOPT_TIMEOUT, 20);
-            
-            $content = curl_exec($ch);
-            
-            if(curl_errno($ch))
-                return false;
-            curl_close($ch);
-            return $content;		
-        }
+//        function check_customer_ln($customerKey,$apiKey){
+//
+//            $url = 'https://login.xecurify.com/moas/rest/customer/license';
+//            $ch = curl_init($url);
+//            //$customerKey = DB::get_option ( 'mo_saml_admin_customer_key' );
+//
+//            //$apiKey = DB::get_option ( 'mo_saml_admin_api_key' );
+//            $currentTimeInMillis = round(microtime(true) * 1000);
+//            $stringToHash = $customerKey . number_format($currentTimeInMillis, 0, '', '') . $apiKey;
+//            $hashValue = hash("sha512", $stringToHash);
+//            $customerKeyHeader = "Customer-Key: " . $customerKey;
+//            $timestampHeader = "Timestamp: " . $currentTimeInMillis;
+//            $authorizationHeader = "Authorization: " . $hashValue;
+//            $fields = '';
+//            $fields = array(
+//                'customerId' => $customerKey,
+//                'applicationName' => 'php_saml_connector_premium_plan'
+//            );
+//            $field_string = json_encode($fields);
+//            curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
+//            curl_setopt( $ch, CURLOPT_ENCODING, "" );
+//            curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+//            curl_setopt( $ch, CURLOPT_AUTOREFERER, true );
+//            curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+//            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);  # required for https urls
+//            curl_setopt( $ch, CURLOPT_MAXREDIRS, 10 );
+//            curl_setopt( $ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json", $customerKeyHeader, $timestampHeader, $authorizationHeader));
+//            curl_setopt( $ch, CURLOPT_POST, true);
+//            curl_setopt( $ch, CURLOPT_POSTFIELDS, $field_string);
+//            curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, 5);
+//            curl_setopt( $ch, CURLOPT_TIMEOUT, 20);
+//
+//            $content = curl_exec($ch);
+//
+//            if(curl_errno($ch))
+//                return false;
+//            curl_close($ch);
+//            return $content;
+//        }
 
         function saml_get_current_domain() {
             $http_host = $_SERVER['HTTP_HOST'];
