b0ae4e9b728edfdae04efa553a1207767ac8643b
sunny <sunny@xecurify.com>
2021-02-11 16:36:58 +0530

    Bumped version to beta 2

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,10 +14,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.4.0',
+    'version' => '1.4.0b',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-10.5.0',
+            'typo3' => '8.7.0-10.6.0',
         ],
         'conflicts' => [],
         'suggests' => [],
